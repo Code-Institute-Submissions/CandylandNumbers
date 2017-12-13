b74166b78dae0f96fb59a9d803989230faf16050
bugdude01 <fafrhodes@yahoo.co.uk>
2017-12-13 18:17:35 +0000

    Connect to MongoDB

--- candyland_Numbers.py
+++ candyland_Numbers.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask import render_template
 from pymongo import MongoClient
@@ -5,14 +6,14 @@ import json
 
 app = Flask(__name__)
 
-MONGODB_HOST = 'localhost'
-MONGODB_PORT = 27017
-DBS_NAME = 'candyNumbers'
-COLLECTION_NAME = 'numbers'
+#MONGODB_HOST = 'localhost'
+#MONGODB_PORT = 27017
+#DBS_NAME = 'new_candyland_numbers'
+COLLECTION_NAME = 'new_candyland_numbers'
 
 
-#MONGO_URI = os.getenv('MONGODB_URI', 'mongodb://localhost:27017')
-#DBS_NAME = os.getenv('MONGO_DB_NAME', 'donorUSA')
+MONGO_URI = os.getenv('MONGODB_URI', 'mongodb://localhost:27017')
+DBS_NAME = os.getenv('MONGO_DB_NAME', 'new_candyland_numbers')
 
 
 @app.route("/")
@@ -23,7 +24,7 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/candyNumbers/numbers")
+@app.route("/new_candyland_numbers")
 def bookings_numbers():
     """
     A Flask view to serve the project data from
@@ -38,7 +39,7 @@ def bookings_numbers():
 
     # Open a connection to MongoDB using a with statement such that the
     # connection will be closed as soon as we exit the with statement
-    with MongoClient(MONGODB_HOST, MONGODB_PORT) as conn:
+    with MongoClient(MONGO_URI) as conn:
         # Define which collection we wish to access
         collection = conn[DBS_NAME][COLLECTION_NAME]
         # Retrieve a result set only with the fields defined in FIELDS
